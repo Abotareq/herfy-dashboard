eeb5af17accd9c5bba2f70268404b5c788d899a1
Minamaher-sw <mosadefmena185@gmail.com>
2025-07-28 11:39:44 +0300

    modify path ** route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './component/main-component/main-component';
+import { Notfound } from './component/notfound/notfound';
 
 export const routes: Routes = [
   { path: '',component: MainComponent,children: []
   },
-  { path: '**', }
+  { path: '**',component:Notfound }
 ];
