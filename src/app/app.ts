755e71c8c22cbfd5c38ddedf13b81c4a570f3817
Minamaher-sw <mosadefmena185@gmail.com>
2025-07-28 11:34:58 +0300

    importing RouterOutlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from "./component/sidebar/sidebar";
 
 @Component({
   selector: 'app-root',
