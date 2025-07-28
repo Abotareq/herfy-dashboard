6744237738574969313cc15a6fe084179ab1acd5
Minamaher-sw <mosadefmena185@gmail.com>
2025-07-28 11:33:02 +0300

    adding main route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './component/main-component/main-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '',component: MainComponent,children: []
+  },
+  { path: '**', redirectTo: '/dashboard' }
+];
